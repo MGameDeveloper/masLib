f2d4ef2cdef177dddc687586777145906e401496
MGameDeveloper <riggingmaker@gmail.com>
2025-08-11 12:52:00 -0700

    .

--- masString.h
+++ masString.h
@@ -22,25 +22,32 @@ struct masString
     int      Capacity;
 };
 
-
-masString* mas_string_create(const masChar* Text)
+masString* mas_string_internal_create(int TextSize)
 {
-    int TextSize   = mas_strlen(Text);
     if(TextSize <= 0)
         return NULL;
     else
-        TextSize++; // NULL terminator need to check if we need it
+        TextSize++; // null terminator need to be proven we may not need it
 
-    int HeaderSize = sizeof(masString);
-    int AllocSize  = HeaderSize + (sizeof(masChar) * TextSize);
-    masString* String = MAS_MALLOC(masString, AllocSize);
+    int AllocSize = sizeof(masString) + (sizeof(masChar) * TextSize);
+    masString* String = (masString*)malloc(AllocSize);
     if(!String)
         return NULL;
     memset(String, 0, AllocSize);
-
-    String->Data     = MAS_PTR_OFFSET(masChar, String, HeaderSize);
+    String->Data     = MAS_PTR_OFFSET(masChar, String, sizeof(masString));
     String->Size     = TextSize;
     String->Capacity = TextSize;
+
+    return String;
+}
+
+masString* mas_string_create(const masChar* Text)
+{
+    masString* String = mas_string_internal_create(MAS_STRLEN(Text));
+    if(!String)
+        return NULL;
+    int CopySize = String->Size - 1;
+    memcpy(String->Data, Text, sizeof(masChar) * CopySize);
 
     return String;
 }
@@ -53,9 +60,9 @@ void mas_string_destroy(masString** String)
     *String = NULL;
 }
 
-void mas_string_insert_first(masString* String, const masChar* Text)
+void mas_string_insert_first(masString** StringRef, const masChar* Text)
 {
-    if(!String)
+    if(!String || !(*String))
         return;
 
     int TextSize = MAS_STRLEN(Text);
@@ -64,27 +71,36 @@ void mas_string_insert_first(masString* String, const masChar* Text)
     else
         TextSize++; // Null terminator may need to check we may not need it
 
-    int RequiredSize = TextSize + String->Size;
-    assert(RequiredSize < MAS_TEMP_BUF_SIZE && "Temporary buffer is smaller than the data being worked on.");
-
-    masChar TempText[MAS_TEMP_BUF_SIZE];
-    memset(TempText, 0, sizeof(masChar) * MAS_TEMP_BUF_SIZE);
-
-    int CopyOffset = TextSize;
-    memcpy(TempText,              Text,         sizeof(masChar) * TextSize);
-    memcpy(TempText + CopyOffset, String->Data, sizeof(masChar) * String->Size);
-
-    if(RequiredSize < String->Capacity)
+    masString *String       = *StringRef;
+    int        RequiredSize = String->Size + TextSize;
+    if(RequiredSize >= String->Capacity)
     {
-        
+        masString *OutString = mas_string_internal_create(RequiredSize);
+        assert(OutString != NULL && "Failed to internally resize string buffer");
+
+        memcpy(OutString->Data,            Text,         sizeof(masChar) * TextSize);
+        memcpy(OutString->Data + TextSize, String->Data, sizeof(masChar) * String->Size);
+
+        mas_string_destroy(StringRef);
+        *StringRef = OutString;
     }
     else
     {
+        assert(RequiredSize < MAS_TEMP_BUF_SIZE && "TempBuf is small for internal operation of strings, consider increasing MAS_TEMP_BUF_SIZE macro.");
 
+        masChar TempBuf[MAS_TEMP_BUF_SIZE];
+        memset(TempBuf, 0, sizeof(masChar) * MAS_TEMP_BUF_SIZE);
+
+        memcpy(OutString->Data,            Text,         sizeof(masChar) * TextSize);
+        memcpy(OutString->Data + TextSize, String->Data, sizeof(masChar) * String->Size);
+
+        memset(String->Data,       0, sizeof(masChar) * String->Size);
+        memcpy(String->Data, TempBuf, sizeof(masChar) * RequiredSize);
+        String->Size = RequiredSize;
     }
 }
 
-void mas_string_insert_last(masString* String, const masChar* Text)
+void mas_string_insert_last(masString** StringRef, const masChar* Text)
 {
     if(!String)
         return;
@@ -93,5 +109,62 @@ void mas_string_insert_last(masString* String, const masChar* Text)
     if(TextSize <= 0)
         return;
     else
-        TextSize++; // Null terminator may need to check we may need it    
+        TextSize++; // Null terminator may need to check we may need it  
+    
+    masString *String       = *StringRef;
+    int        RequiredSize = String->Size + TextSize;
+    if(RequiredSize >= String->Capacity)
+    {
+        masString *OutString = mas_string_internal_create(RequiredSize);
+        assert(OutString != NULL && "Failed to internally resize string buffer");
+
+        memcpy(OutString->Data,                String->Data, sizeof(masChar) * String->Size);
+        memcpy(OutString->Data + String->Size, Text,         sizeof(masChar) * TextSize);
+
+        mas_string_destroy(StringRef);
+        *StringRef = OutString;
+    }
+    else
+    {
+        assert(RequiredSize < MAS_TEMP_BUF_SIZE && "TempBuf is small for internal operation of strings, consider increasing MAS_TEMP_BUF_SIZE macro.");
+
+        masChar TempBuf[MAS_TEMP_BUF_SIZE];
+        memset(TempBuf, 0, sizeof(masChar) * MAS_TEMP_BUF_SIZE);
+
+        memcpy(OutString->Data,                String->Data, sizeof(masChar) * String->Size);
+        memcpy(OutString->Data + String->Size, Text,         sizeof(masChar) * TextSize);
+
+        memset(String->Data,       0, sizeof(masChar) * String->Size);
+        memcpy(String->Data, TempBuf, sizeof(masChar) * RequiredSize);
+        String->Size = RequiredSize;
+    }
+}
+
+void mas_string_insert_at(masString** StringRef, int Offset, const masChar* Text)
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/**************************************************************************************************************************
+* EXAMPLE
+***************************************************************************************************************************/
+int main(int argc, const char** argv)
+{
+    masString* IntroMsg = mas_string_create(L"Glad you choosen to be on of the");
+    mas_string_insert_last(&IntroMsg, L" immortals who loves to be on the verge of destruction.");
+    mas_string_insert_first(&IntroMsg, L"Welcome to the masEngine, we are ");
+    mas_string_insert_replace(&IntroMsg, L"Glad you", L"glad you've");
+    mas_string_remove(Intro, )
+    return 0;
 }
