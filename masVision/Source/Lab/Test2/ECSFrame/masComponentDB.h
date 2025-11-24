c034b5471207402f55b525e93a482f13fa23fade
MGameDev <riggingmaker@gmail.com>
2025-11-24 23:07:48 +0300

    ..

--- masVision/Source/Lab/Test2/ECSFrame/masComponentDB.h
+++ masVision/Source/Lab/Test2/ECSFrame/masComponentDB.h
@@ -1,87 +1,401 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAS_INVALID_VALUE 0xFFFFFFFF
 
-typedef struct _masComponent
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define MAS_COMPONENT_DB_TAG              (('C' << 24) | ('M' << 16) | ('P' << 8) | ('S' << 0))
+#define MAS_PTR_OFFSET(type, ptr, offset) (type*)(((uint8_t*)ptr) + offset)
+
+#define MAS_COMPONENT_DB_NAME           "masComponent.masDB"
+#define MAS_COMPONENT_DB_BACKUP_NAME    "masComponent.masDB_Backup"
+#define MAS_COMPONENT_DB_TEMP_NAME      "masComponent.masDB_Temp"
+#define MAS_COMPONENT_DB_CORRUPTED_NAME "masComponent.masDB_Corrupted"
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+typedef struct masFileContent
 {
-	uint32_t GUID;       // Generated id for this specific component saved with it on desc when db is written out
-	uint32_t Size;       // size of the compoennt at runtime
-	uint32_t NameOffset; // Offset into the name buffer
-} masComponent;
+	void* Data;
+	uint32_t Size;
+};
 
-typedef struct _masComponentName
-{ 
-	uint8_t  Len;     // len of this name start from Name[1]
-	char     Name[1]; // first letter of the name of this component
-} masComponentName;
-
-// Name of component would be hashed to be used as index to one these
-typedef struct _masComponentHashEntry
+bool masFileContent_Load(masFileContent* FileContent, const char* Path)
 {
-	uint64_t Hash;       // used to check if collision happenes
-	uint32_t DescIdx;    // index into components
-} masComponentHashEntry;
+	FILE* File = fopen(Path, "rb");
+	if (!File)
+		return false;
 
-typedef struct _masComponentDB
+	fseek(File, 0, SEEK_END);
+	uint32_t FileSize = (uint32_t)_ftelli64(File);
+	fseek(File, 0, SEEK_SET);
+	if (FileSize <= 0)
+	{
+		fclose(File);
+		return false;
+	}
+	
+	void* Data = malloc((size_t)FileSize);
+	if (!Data)
+	{
+		fclose(File);
+		return false;
+	}
+
+	size_t ReadCount = fread(Data, 1, FileSize, File);
+	if (ReadCount != FileSize)
+	{
+		fclose(File);
+		free(Data);
+		return false;
+	}
+
+	fclose(File);
+
+	FileContent->Data = Data;
+	FileContent->Size = FileSize;
+	return true;
+}
+
+void masFileContent_UnLoad(masFileContent* FileContent)
 {
-	masComponent          *Components;   // component desc meta data
-	masComponentName      *Names;   // string buffer and compoent desc has offset into for its name
-	masComponentHashEntry *Entries; // hash table entryies array
-	uint32_t ComponentNextGUID;     // Used every time a component is add to give it a unique id 
-	uint32_t ComponentCapacity;          // max components we can store
-	uint32_t ComponentCount;             // current component count
-	uint32_t EntryCapacity;         // max hash table entries 
-	uint32_t EntryCount;            // current entry count
-	uint32_t NameCapacity;          // entire buffer size
-	uint32_t NameOffset;            // current offset into buffer can be used to check if any size left in the buffer to resize if needed
+	if (!FileContent)
+		return;
+
+	if(FileContent->Data)
+		free(FileContent->Data);
+	FileContent->Data = NULL;
+	FileContent->Size = 0;
+}
+
+bool masFileContent_IsValid(masFileContent* FileContent)
+{
+	if (!FileContent || !FileContent->Data || FileContent->Size == 0)
+		return false;
+	return true;
+}
+
+bool masFileContent_WriteToDisk(masFileContent* FileContent, const char* Path)
+{
+	if (!masFileContent_IsValid(FileContent))
+		return false;
+
+	FILE* File = fopen(Path, "wb");
+	if (!File)
+		return false;
+	
+	size_t WriteByte = fwrite(FileContent->Data, 1, FileContent->Size, File);
+	if (WriteByte != FileContent->Size)
+	{
+		// raise error
+		fclose(File);
+		return false;
+	}
+
+	if (fflush(File) == EOF)
+	{
+		fclose(File);
+		return false;
+	}
+
+	fclose(File);
+	return true;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool masFile_Exists(const char* Path)
+{
+	FILE* File = fopen(Path, "rb");
+	if (!File)
+		return false;
+	fclose(File);
+	return true;
+}
+
+bool masFile_Remove(const char* Path)
+{
+	if (remove(Path) != 0)
+		return false;
+	return true;
+}
+
+bool masFile_Rename(const char* CurrentName, const char* NewName)
+{
+	if (rename(CurrentName, NewName) != 0)
+		return false;
+
+	return true;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+* masComponent: Describe a component
+* 
+* ID:   Unique ID of this component
+* Size: Memory required size for this component
+* 
+* Name: Since names' length vary doing fixed size name would cause waste of memory OR limit some name's if fixed name size is smaller
+*       so all names live in a seperate block of memory where we only have:
+*       - Offset: Offset from the start of name's block memory
+*       - Length: Name's Length including NULL terminator
+*/
+typedef struct masComponent
+{
+	uint32_t ID;
+	uint32_t Size;
+	struct
+	{
+		uint32_t Offset;
+		uint32_t Length;
+	} Name;
 };
 
 
-void masComponentDB_CreateOrLoad();
-void masComponentDB_SaveAndDestroy();
-
-
-typedef struct _masComponentDesc
+/*
+* masComponentEntry: Component's Name is hashed and truned to index to one of these
+* 
+* Hash:    Result of hashing the component name USED to detect hashing collision
+* CompIdx: Index into masComponent array
+* _Pad:    Padding the structure manually
+*/
+typedef struct masComponentEntry
 {
-	const char *Name;
-	uint32_t    Size;
-} masComponentDesc;
-#define MAS_COMP(Comp) { #Comp, sizeof(Comp)}
+	uint64_t Hash;     
+	uint32_t CompIdx;
+	uint32_t _Pad;
+};
 
-typedef union _masVec2 
+
+/*
+* masComponentDBHeader: Describe data base on disk and where the data can be found within the file
+* 
+* Tag:                  Magic number to distinguish data base of component from other files
+* Version:              Has many use cases like detecting different implementation of the db[ currently not important ]
+* ComponentGUID:        Keeps increasing to give every add component a unique id so they can be the same across machines and on different runs
+* ComponentBlock:       Tells us where to find components' list on disk relative to the file
+* NameBlock:            Tells us where to find components' name buffer on disk relative to the file
+* ComponentEntryBlockc: Tells us where to find hash table for components on disk relative to the file
+*/
+typedef struct masComponentDBHeader
 {
-	float xy[2];
+	uint32_t Tag;
+	uint32_t Version;
+	uint32_t ComponentGUID;
+	uint32_t FileSize;
+
 	struct
 	{
-		float x, y;
-	};
-} masVec2;
+		uint32_t Capacity;
+		uint32_t Count;
+		uint32_t DBOffset;
+		uint32_t MemSize;
+	} ComponentList;
 
-
-
-void masComponentDB_Add(masComponentDesc *DescList, uint32_t Count)
-{
-	uint64_t ComponentListHash = 0;
-	for (int32_t i = 0; i < Count; ++i)
+	struct
 	{
-		uint64_t CompHash = masComponentDescHash(&Desc[i]);
-		uint32_t EntryIdx = CompHash % masComponentDB_HashTableCapacity();
+		uint32_t Capacity;
+		uint32_t Count;
+		uint32_t DBOffset;
+		uint32_t MemSize;
+	} NameBuffer;
 
-		masComponentHashEntry* Entry = masComponentDB_HashEntry(EntryIdx);
-		if (Entry->Hash == CompHash)
+	struct
+	{
+		uint32_t Capacity;
+		uint32_t Count;
+		uint32_t ResizeCounter;
+		uint32_t DBOffset;
+		uint32_t MemSize;
+		float    LoadRatio;
+	} ComponentEntryTable;
+};
+
+
+/*
+* masComponentDB: Loaded components' data base from disk
+* 
+* Header:              Parsed header where it has all data required to create or find data in the db
+* ComponentList:       Pointer acquired after Mapping/Loading the data base from disk
+* ComponentEntryTable: Hash table to find components' by their names
+*/
+typedef struct masComponentDB
+{
+	masFileContent DBFile;
+
+	masComponentDBHeader *Header;
+	masComponent         *ComponentList;
+	masComponentEntry    *ComponentEntryTable;
+	char                 *NameBuffer;
+};
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+static masComponentDB GComponentDB = { };
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool masComponentDBInternal_Create()
+{
+	uint32_t InitCompCapacity       = 256;
+	uint32_t InitNameBufferCapacity = 1024;
+	uint32_t InitCompEntryCapacity  = 1024;
+
+	uint32_t DBFileSize = 
+		 sizeof(masComponentDBHeader)                       + 
+		(sizeof(masComponent)      * InitCompCapacity)      +
+		(sizeof(masComponentEntry) * InitCompEntryCapacity) + 
+		InitNameBufferCapacity;
+
+	void *DBData = malloc(DBFileSize);
+	if (!DBData)
+		return false;
+	memset(DBData, 0, DBFileSize);
+
+	masComponentDBHeader* Header = MAS_PTR_OFFSET(masComponentDBHeader, DBData, 0);
+	Header->Tag                          = MAS_COMPONENT_DB_TAG;
+	Header->Version                      = 1;
+	Header->ComponentGUID                = 1;
+	Header->FileSize                     = DBFileSize;
+
+	Header->ComponentList.Capacity       = InitCompCapacity;
+	Header->ComponentList.Count          = 0;
+	Header->ComponentList.DBOffset       = sizeof(masComponentDBHeader);
+	Header->ComponentList.MemSize        = InitCompCapacity * sizeof(masComponent);
+
+	Header->NameBuffer.Capacity          = InitNameBufferCapacity;
+	Header->NameBuffer.Count             = 0;
+	Header->NameBuffer.DBOffset          = Header->ComponentList.DBOffset + Header->ComponentList.MemSize;
+	Header->NameBuffer.MemSize           = sizeof(char) * InitNameBufferCapacity;
+
+	Header->ComponentEntryTable.Capacity = InitCompEntryCapacity;
+	Header->ComponentEntryTable.Count    = 0;
+	Header->ComponentEntryTable.DBOffset = Header->NameBuffer.DBOffset + Header->NameBuffer.MemSize;
+	Header->ComponentEntryTable.MemSize  = sizeof(masComponentEntry) * InitCompEntryCapacity;
+
+	GComponentDB.DBFile.Data         = DBData;
+	GComponentDB.DBFile.Size         = DBFileSize;
+	GComponentDB.Header              = MAS_PTR_OFFSET(masComponentDBHeader, GComponentDB.DBFile.Data, 0);
+	GComponentDB.ComponentList       = MAS_PTR_OFFSET(masComponent,         GComponentDB.DBFile.Data, Header->ComponentList.DBOffset);
+	GComponentDB.NameBuffer          = MAS_PTR_OFFSET(char,                 GComponentDB.DBFile.Data, Header->NameBuffer.DBOffset);
+	GComponentDB.ComponentEntryTable = MAS_PTR_OFFSET(masComponentEntry,    GComponentDB.DBFile.Data, Header->ComponentEntryTable.DBOffset);
+
+	return true;
+}
+
+bool masComponentDBInternal_Load()
+{
+	if (!masFileContent_IsValid(&GComponentDB.DBFile) || GComponentDB.DBFile.Size <= sizeof(masComponentDBHeader))
+		return false;
+
+	masComponentDBHeader *Header = MAS_PTR_OFFSET(masComponentDBHeader, GComponentDB.DBFile.Data, 0);
+	if (Header->Tag != MAS_COMPONENT_DB_TAG)
+	{
+		// raise an error file is not component data base
+		return false;
+	}
+
+	// Header.Version; // use version if needed
+
+	GComponentDB.Header              = MAS_PTR_OFFSET(masComponentDBHeader, GComponentDB.DBFile.Data, 0);
+	GComponentDB.ComponentList       = MAS_PTR_OFFSET(masComponent,         GComponentDB.DBFile.Data, Header->ComponentList.DBOffset);
+	GComponentDB.NameBuffer          = MAS_PTR_OFFSET(char,                 GComponentDB.DBFile.Data, Header->NameBuffer.DBOffset);
+	GComponentDB.ComponentEntryTable = MAS_PTR_OFFSET(masComponentEntry,    GComponentDB.DBFile.Data, Header->ComponentEntryTable.DBOffset);
+
+	return true;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool masComponentDB_Create()
+{
+	return masComponentDBInternal_Create();
+}
+
+bool masComponentDB_Load()
+{
+	if (!masFileContent_Load(&GComponentDB.DBFile, MAS_COMPONENT_DB_NAME))
+		return false;
+
+	if (!masComponentDBInternal_Load())
+	{
+		masFileContent_UnLoad(&GComponentDB.DBFile);
+		return false;
+	}
+
+	return true;
+}
+
+// NEED REFACTORY AND BETTER HANDLING THE PROBLEM
+// MAY NEED TO IMPLEMENT FILE I/O IN windows win32 api
+bool masComponentDB_Save()
+{
+	if (!masFileContent_IsValid(&GComponentDB.DBFile))
+		return false;
+
+	// check crc for current db with the one on disk if they are the same means no change return true
+	// if not craete temp file copy the data base to it and calculate the crc for it
+	// after complete write delete previouse and rename it
+
+	if (!masFileContent_WriteToDisk(&GComponentDB.DBFile, MAS_COMPONENT_DB_TEMP_NAME))
+	{
+		if (masFile_Exists(MAS_COMPONENT_DB_TEMP_NAME))
 		{
-			// already added get stored one
+			masFile_Rename(MAS_COMPONENT_DB_TEMP_NAME, MAS_COMPONENT_DB_CORRUPTED_NAME);
 		}
-		else if (Entry->Hash != CompHash && Entry->Hash != CompHash)
+		
+		return false;
+	}
+	
+	if (masFile_Exists(MAS_COMPONENT_DB_BACKUP_NAME))
+	{
+		if (!masFile_Remove(MAS_COMPONENT_DB_BACKUP_NAME))
 		{
-			// collision happens same entry but different compoennts
-		}
-		else if (Entry->Hash == MAS_INVALID_VALUE)
-		{
-			// create new component
-			// give it new GUID from masComponentDB_NextGUID();
+			masFile_Rename(MAS_COMPONENT_DB_TEMP_NAME, MAS_COMPONENT_DB_CORRUPTED_NAME);
+			// raise error
+			return false;
 		}
 	}
+
+	if (masFile_Rename(MAS_COMPONENT_DB_NAME, MAS_COMPONENT_DB_BACKUP_NAME))
+	{
+		masFile_Rename(MAS_COMPONENT_DB_TEMP_NAME, MAS_COMPONENT_DB_NAME);
+	}
+	else
+	{
+		// rais error
+		return false;
+	}
+
+	return true;
 }
+
+
+void masComponentDB_Destroy()
+{
+	masFileContent_UnLoad(&GComponentDB.DBFile);
+	memset(&GComponentDB, 0, sizeof(masComponentDB));
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
